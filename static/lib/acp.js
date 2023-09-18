e7a75b6fc3e340a58e28ea405639f2260510156c
Julian Lam <julian@nodebb.org>
2023-09-18 11:29:17 -0400

    chore: remove debug log

--- static/lib/acp.js
+++ static/lib/acp.js
@@ -4,7 +4,6 @@
 	const hooks = await app.require('hooks');
 
 	hooks.on('filter:script.load', ({ tpl_url, scripts }) => {
-		console.log('am here', tpl_url);
 		if (tpl_url === 'admin/plugins/sso-oauth2-multiple-v2') {
 			scripts.splice(scripts.indexOf('forum/admin/plugins/sso-oauth2-multiple-v2'), 1, 'admin/plugins/sso-oauth2-multiple');
 		}
